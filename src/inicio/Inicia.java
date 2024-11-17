68a31a24c4514bdac7990a8f44d08dd959c4c2ff
Bartolomeu Spegiorin Gusella <ccbarto@gmail.com>
2024-11-17 19:03:52 -0300

    começo da criação dos perfis

--- src/inicio/Inicia.java
+++ src/inicio/Inicia.java
@@ -5,33 +5,48 @@ import java.util.Scanner;
 
 import dados.ArquivoTipo;
 import dados.GerenciaArquivos;
+import dados.repositorio.UsuarioRepositorio;
+import pessoas.Usuario;
 
 public class Inicia {
     public static void main(String[] args) {
         System.out.println("Bem Vindo ao Sistema Bancário BML");
 
-        // Scanner scan = new Scanner(System.in);
-        // loginUI(scan);
+        Scanner scan = new Scanner(System.in);
+        Usuario user = loginUI(scan);
 
-        GerenciaArquivos gerencia = new GerenciaArquivos(ArquivoTipo.USUARIO);
+        // GerenciaArquivos gerencia = new GerenciaArquivos(ArquivoTipo.USUARIO);
 
-        ArrayList<String> linhas = new ArrayList<String>();
-        linhas.add("Bart;bart@bart.net.br;senha123;11111111111");
+        // ArrayList<String> linhas = new ArrayList<String>();
+        // linhas.add("Bart;bart@bart.net.br;senha123;11111111111");
         
-        gerencia.salvaDados(linhas);
+        // gerencia.salvaDados(linhas);
 
         System.out.println("cabou");
 
         
     }
 
-    public static boolean loginUI(Scanner scan){
-        System.out.println("Digite o cpf:");
-        String cpfTentativa = scan.nextLine();
-        System.out.println("Digite a senha:");
-        String senhaTentativa = scan.nextLine();
-        System.out.printf("Bem Vindo %s", "nomeDoUsuário");
+    public static Usuario loginUI(Scanner scan){
 
-        return true;
+        int tentativas = 0;
+        int MAX_TENTATIVAS = 3;
+        while (tentativas < MAX_TENTATIVAS) {
+            System.out.println("Digite o email:");
+            String emailTentativa = scan.nextLine();
+            System.out.println("Digite a senha:");
+            int senhaTentativa = scan.nextInt();
+            System.out.printf("Bem Vindo %s", "nomeDoUsuário");
+    
+            UsuarioRepositorio usuarioRepositorio = new UsuarioRepositorio();
+            try {
+                return usuarioRepositorio.findByEmailAndPassword(emailTentativa, senhaTentativa);
+            } catch (Exception e) {
+                System.out.println("Opss usuário e senha errados, por favor tente novamente !!");
+                tentativas ++;
+            }
+        }
+
+        throw new Error("Numero de tentativas excedidas, suas informações estão sendo coletadas para averiguação !!");
     }
 }
---
+++ src/inicio/MenuSistema.java
@@ -0,0 +1,33 @@
+package inicio;
+
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
+
+import pessoas.enums.TipoUsuario;
+
+public enum MenuSistema {
+    CADASTRAR_CORRENTISTA("CADASTRAR_CORRENTISTA", EnumSet.of(TipoUsuario.GERENTE)), 
+    CONFIGURAR_LIMITE_CONTA("CONFIGURAR_LIMITE_CONTA",EnumSet.of(TipoUsuario.GERENTE)),
+    REALIZAR_SAQUE("REALIZAR_SAQUE",EnumSet.of(TipoUsuario.BANCARIO, TipoUsuario.GERENTE)),
+    REALIZAR_DEPOSITO("REALIZAR_DEPOSITO",EnumSet.of(TipoUsuario.BANCARIO, TipoUsuario.GERENTE)),
+    REALIZAR_TRANSFERENCIA("REALIZAR_TRANSFERENCIA",EnumSet.of(TipoUsuario.BANCARIO, TipoUsuario.GERENTE))
+
+    private Set<TipoUsuario> permissoes;
+    private String nome;
+
+    MenuSistema(String nome, Set<TipoUsuario> tipos){
+        this.nome = nome;
+        this.permissoes = tipos;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public Set<TipoUsuario> getPermissoes() {
+        return permissoes;
+    }
+
+    public boolean temAcesso(TipoUsuario tipo){
+        return permissoes.contains(tipo);
+    }
+}
