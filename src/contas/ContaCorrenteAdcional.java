b88ca423d5a2cd43ae0f41b3f9f13bdb8b9f9482
Luan Santos Cavalcante <luansantos1802@gmail.com>
2024-11-21 20:42:15 -0300

    criacao das classes de contas

--- src/contas/ContaCorrenteAdcional.java
+++ src/contas/ContaCorrenteAdcional.java
@@ -1,5 +1,56 @@
 package contas;
 
-public class ContaCorrenteAdcional {
+public class ContaCorrenteAdcional extends Contaprincipal{
+	
+	// declarando paramentros conta corrente adicional
+		private double limite;
+		private int numeroDaContaPrincipal;
+		
+		
+		
+
+		public ContaCorrenteAdcional (String CorrentistaTitular, int Numerodaconta, double limite, int numeroDaContaPrincipal) {
+				super (TipoCorrentista.DEPENDENTE, "Conta corrente adicional", Numerodaconta);
+				this.limite = limite;
+				this.numeroDaContaPrincipal = numeroDaContaPrincipal;
+			
+		}
+		
+		@Override
+		
+		public void sacar (double valor) {
+			if (valor <= 0) {
+				throw new IllegalArgumentException ("Valor para saque deve ser possitivo");
+		
+			}
+			
+			if (saldo < valor && limite < valor) {
+				throw new IllegalArgumentException ("Saldo ou limite insuficiente");
+
+			}
+		
+		
+		
+		}
+		
+		
+		
+		public int getNumeroDaContaPrincipal() {
+			return numeroDaContaPrincipal;
+		}
+
+		public void setNumeroDaContaPrincipal(int numeroDaContaPrincipal) {
+			this.numeroDaContaPrincipal = numeroDaContaPrincipal;
+		}
+
+		public double getLimite() {
+			return limite;
+		}
+
+		public void setLimite(double limite) {
+			this.limite = limite;
+		}
+
+	
 
 }
--- src/contas/ContaPoupanca.java
+++ src/contas/ContaPoupanca.java
@@ -1,5 +1,42 @@
 package contas;
 
-public class ContaPoupanca {
-
+public class ContaPoupanca extends Contaprincipal {
+	// criando paramentos conta poupança
+	private double rendimentoMensal;
+	private int periodoSemJuros; // numero do tempo de ultimo calculo de juros
+	
+	public ContaPoupanca(int NumerodaConta, String CorrentistaTitular, double rendimentoMensal, int periodoSemJuros) {
+		super(TipoCorrentista.TITULAR, "Conta poupança", NumerodaConta);
+		this.periodoSemJuros = 0;
+		this.rendimentoMensal = 0.05;
+	}
+	public void calcularjuros(double juros) {
+		juros = saldo * rendimentoMensal * periodoSemJuros;
+		saldo += juros;
+		periodoSemJuros = 0;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public double getRendimentoMensal() {
+		return rendimentoMensal;
+	}
+	public void setRendimentoMensal(double rendimentoMensal) {
+		this.rendimentoMensal = rendimentoMensal;
+	}
+	public int getPeriodoSemJuros() {
+		return periodoSemJuros;
+	}
+	public void setPeriodoSemJuros(int periodoSemJuros) {
+		this.periodoSemJuros = periodoSemJuros;
+	}
+	
 }
